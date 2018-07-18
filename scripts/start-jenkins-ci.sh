565c46e37b54fadb6c6ca9a9d19f10a005d92447
faulty <faulty.lee@gmail.com>
2018-07-18 21:51:42 +0800

    simpler travis-ci and refactored sh script

--- scripts/start-jenkins-ci.sh
+++ scripts/start-jenkins-ci.sh
@@ -4,16 +4,27 @@ if [ -e ../.env ]; then
   source ../.env
 fi
 
-# Get the previous Travis-CI IP and remove from Security Group
-IP=$(aws ec2 describe-security-groups --group-ids $JENKINS_SECURITY_GROUP_ID --query "SecurityGroups[*].IpPermissions[*].IpRanges[?Description=='Travis-CI'].CidrIp" --output text)
+function check_and_trigger_build(){
+  if [[ "$(curl --connect-timeout 5 -s -o /dev/null -w ''%{http_code}'' http://$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io/jenkins/)" == "200" ]]; then
+    curl -s -X POST http://$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io/jenkins/job/ngip/job/$TRAVIS_BRANCH/build?delay=0sec
+    exit 0
+  fi
+}
 
-if [ -n $IP ]; then
+function docker_run_aws_cli(){
   docker run --rm \
     --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
     garland/aws-cli-docker \
-    aws ec2 revoke-security-group-ingress \
+    aws $@
+}
+
+# Get the previous Travis-CI IP and remove from Security Group
+IP=$(aws ec2 describe-security-groups --group-ids $JENKINS_SECURITY_GROUP_ID --query "SecurityGroups[*].IpPermissions[*].IpRanges[?Description=='Travis-CI'].CidrIp" --output text)
+
+if [ -n $IP ]; then
+    docker_run_aws_cli ec2 revoke-security-group-ingress \
       --group-id $JENKINS_SECURITY_GROUP_ID \
       --port 80 \
       --protocol tcp --cidr $IP;
@@ -22,23 +33,13 @@ fi
 # Add the current Travis-CI IP into the Security Group
 IP=$(curl -s ifconfig.co)
 if [ -n $IP ]; then
-  docker run --rm \
-    --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-    garland/aws-cli-docker \
-    aws ec2 authorize-security-group-ingress \
+  docker_run_aws_cli ec2 authorize-security-group-ingress \
       --group-id $JENKINS_SECURITY_GROUP_ID \
       --protocol tcp \
       --port 80 \
       --cidr $IP/32
 
-  docker run --rm \
-    --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-    garland/aws-cli-docker \
-    aws ec2 update-security-group-rule-descriptions-ingress \
+  docker_run_aws_cli ec2 update-security-group-rule-descriptions-ingress \
       --group-id $JENKINS_SECURITY_GROUP_ID \
       --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 80, \"ToPort\": 80, \"IpRanges\": [{\"CidrIp\": \"$IP/32\", \"Description\": \"Travis-CI\"}]}]"
 
@@ -50,12 +51,7 @@ fi
 # if Jenkins CI Server already running, trigger tge build straight away
 check_and_trigger_build
 
-docker run --rm \
-  --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-  garland/aws-cli-docker \
-  aws ec2 start-instances --instance-ids $JENKINS_INSTANCE_ID
+docker_run_aws_cli ec2 start-instances --instance-ids $JENKINS_INSTANCE_ID
 
 
 # Wait for Jenkins CI Server to start, should be less than 2~4 mins
@@ -63,7 +59,7 @@ counter=0
 result=0
 until [ $counter -ge 24 ]
 do
-  [[ "$(curl --connect-timeout 5 -s -o /dev/null -w ''%{http_code}'' http://build.ngip.io/jenkins) --user $NGIP_BUILD_USER" == "302" ]] && result=1 && break
+  [[ "$(curl --connect-timeout 5 -s -o /dev/null -w ''%{http_code}'' http://$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io/jenkins/)" == "200" ]] && result=1 && break
   counter=$[$counter+1]
   printf '.'
   sleep 5
@@ -74,10 +70,3 @@ if [ $result -eq 0 ]; then
 fi
 
 check_and_trigger_build
-
-function check_and_trigger_build(){
-  if [[ "$(curl --connect-timeout 5 -s -o /dev/null -w ''%{http_code}'' http://build.ngip.io/jenkins) --user $NGIP_BUILD_USER" == "302" ]]; then
-    curl -s http://build.ngip.io/jenkins/job/ngip/build?token=hfw8yF34JWsWAPnBZDzItipoio1z1OgI --user $NGIP_BUILD_USER
-    exit 0
-  fi
-}
