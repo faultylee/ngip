40497f2fdd90c8e112d93100b0d5f1bc11e9ab75
faulty <faulty.lee@gmail.com>
2018-07-25 19:48:10 +0800

    setup telegram message

--- scripts/check-for-idle-and-shutdown-jenkins.sh
+++ scripts/check-for-idle-and-shutdown-jenkins.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash -xe
+#!/bin/bash -xe
 # This script checks if the Jenkins CI server is idle or 60mis has pass since the last build, it will shutdown the server
 
 if [ -e ../.env ]; then
@@ -8,7 +8,7 @@ fi
 source ./docker_helper.sh
 
 keep_jenkins=false
-
+message=
 #for url in $(docker_run curl -s http://$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io/jenkins/api/json | docker_run jq -r ".jobs[]? | .url"); do
   #for job_url in $(docker_run curl -s "${url/build.ngip.io/$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io}api/json" | docker_run jq -r ".jobs[]? | .url"); do
 
@@ -18,12 +18,18 @@ keep_jenkins=false
     last_build_url=$(docker_run curl -s ${job_url/build.ngip.io/$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io}api/json | docker_run_i jq -r ".lastBuild? | .url")
     if [ -n "$last_build_url" ]; then
       timestamp=$[($(date +%s)*1000 - $(docker_run curl -s "${last_build_url/build.ngip.io/$JENKINS_API_USERNAME:$JENKINS_API_TOKEN@build.ngip.io}api/json" | docker_run_i jq -r ".timestamp"))/1000]
-      if [ $timestamp -lt $[60 * 60] ]; then
+      remaining=$[$JENKINS_SHUTDOWN_IDLE_SEC - $timestamp]
+      message=$"$message $last_build_url remaining time: $remaining\n"
+      if [ $timestamp -lt $JENKINS_SHUTDOWN_IDLE_SEC ]; then
         keep_jenkins=true
       fi
     fi
   done
 #done
+
+if [ -n "$message" ]; then
+  docker_run curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage --data-urlencode "$message" --data "chat_id=$TELEGRAM_CHAT_ID"
+fi
 
 if [ "$keep_jenkins" = false ]; then
   docker_run aws ec2 stop-instances --instance-ids $JENKINS_INSTANCE_ID
