a114de709d9a67cc5354d7a4db1efd288b327850
faulty <faulty.lee@gmail.com>
2018-08-08 23:02:48 +0800

    fix docker run script and added volume

--- scripts/docker_helper.sh
+++ scripts/docker_helper.sh
@@ -2,6 +2,7 @@
 
 function docker_run(){
   docker run --rm --net="host"\
+    --volume $(pwd):/data \
     --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
@@ -11,6 +12,7 @@ function docker_run(){
 
 function docker_run_i(){
   docker run --rm -i --net="host"\
+    --volume $(pwd):/data \
     --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     --env AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
